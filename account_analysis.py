908e969d379462b9c158a87681418d3a067c2be7
BilgeBengisu <76906763+BilgeBengisu@users.noreply.github.com>
2024-07-02 02:51:39 -0400

    setting up for account sort

--- account_analysis.py
+++ account_analysis.py
@@ -7,10 +7,16 @@ import pandas as pd
 # - tourism & business
 # - other (public)
 
-# the labeled accounts from the red tide research is being utilized and 
-# the missing accounts are identified with the ML algorithm implemented previously for the red tide research. 
+# the labeled accounts from the red tide research is being utilized
+accounts = pd.read_csv("Final_Account_Labels_for_Dashboard.csv")
+# and the new accounts will be labeled with ML algorithm from red tide research
 
-nort_oil = pd.read_csv("spill_data/Cleaned_NORT_Files/C_NORT_Oil.csv")
+#starting to practice with a small dataset before running on all spills combined
+nort_oil = pd.read_csv("spill_data/Cleaned_NORT_Files/C_NORT_Oil.csv", usecols=["username", "description"])
+print(nort_oil)
+nort_oil['Label'] = 'yet'
+nort_oil['Label.Type'] = 'yet'
 
+print(nort_oil)
 
 # saving the users that are yet to be labeled
