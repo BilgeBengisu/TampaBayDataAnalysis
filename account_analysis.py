5cc1fd331358daf9ff30dfa2878439c83bb47869
BilgeBengisu <76906763+BilgeBengisu@users.noreply.github.com>
2024-07-03 14:39:16 -0400

    ML model files

--- account_analysis.py
+++ account_analysis.py
@@ -57,3 +57,60 @@ existing_usernames = set(accounts['username'])
 users_to_be_labelled = spill_merged[~spill_merged['username'].isin(existing_usernames)]
 
 print(users_to_be_labelled)
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import cross_val_predict, train_test_split, GridSearchCV
+from sklearn.svm import SVC
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
+
+from nltk.corpus import wordnet as wn
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk import word_tokenize, pos_tag
+from collections import defaultdict
+import re
+import json
+from sklearn.metrics import confusion_matrix
+import pickle
+
+tag_map = defaultdict(lambda: wn.NOUN)
+tag_map['J'] = wn.ADJ
+tag_map['V'] = wn.VERB
+tag_map['R'] = wn.ADV
+tag_map['AS'] = wn.ADJ_SAT
+
+# filepath = "finalized_8K_accounts.csv"
+# filepath = "UNLABELED_accounts_emojis_replaced.csv"
+filepath = "spill_data/Account/FINALIZED_Unlabeled_Data_ALL_Available_Descriptions_EMOJIS_UNCHANGED.csv"
+hand_label = "hand.label"
+government = "gov"
+academia = "acad"
+tourBiz = "tourbiz"
+
+df = pd.read_csv(filepath)
+
+# df = df[((df[hand_label] == 'media') | (df[hand_label] == tourBiz) |(df[hand_label] == academia) | (df[hand_label] == government) | (
+#        df[hand_label] == 'other'))]
+
+df = df[['username', 'description']]  # keep only relevant columns
+
+lemmatizer = WordNetLemmatizer()
+words_not_changed = ['media']
+
+
+def preprocessing(row):
+    if str(row) == "nan":
+        lemma = ""
+    else:
+        row = str(row).lower()
+        row = word_tokenize(row)  # tokenize
+        lemma = [lemmatizer.lemmatize(token, tag_map[tag[0]]) if token not in words_not_changed else token for
+                 token, tag in pos_tag(row)]  # lemmatization, depending on part-of-speech
+        lemma = ["" if re.search(r'\b[0-9]+\b\s*', lem) else lem for lem in lemma]  # removing
+    return str(lemma)
+
+
+df['description_lemmatized'] = df['description'].apply(preprocessing)
